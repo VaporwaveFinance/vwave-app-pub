d004252a4a55b8d3cd089e2630a37d672beb6d54
VaporwaveFinance <95114728+VaporwaveFinance@users.noreply.github.com>
2023-02-03 13:37:15 -0500

    Update mantle_pools.js

--- src/features/configure/vault/mantle_pools.js
+++ src/features/configure/vault/mantle_pools.js
@@ -1,15 +1,15 @@
-export const auroraPools = [
+export const mantlePools = [
  {
     id: 'mantleTestnet-bit-test',
     name: 'BIT-TEST PhotonLP',
     token: 'BIT-TEST PhotonLP',
     tokenDescription: 'Photonswap',
-    tokenAddress: '0xBBf3D4281F10E537d5b13CA80bE22362310b2bf9', //  wantToken
+    tokenAddress: '0x36047876cd5d68C58d1972fd567b6edc3788F93a', //  wantToken
     tokenDecimals: 18,
     tokenDescriptionUrl: '#',
     earnedToken: 'vapor-BIT-TEST-photonswap',
-    earnedTokenAddress: '0xf2aa2Df5b94B5e19aCac158BC0ee1945eB5DdC08', //  vault
-    earnContractAddress: '0xf2aa2Df5b94B5e19aCac158BC0ee1945eB5DdC08', //  vault
+    earnedTokenAddress: '0xcDB200B954180505B03207aB1b5ff7e01cdf5faD', //  vault
+    earnContractAddress: '0xcDB200B954180505B03207aB1b5ff7e01cdf5faD', //  vault
     pricePerFullShare: 1,
     tvl: 0,
     oracle: 'lps',
