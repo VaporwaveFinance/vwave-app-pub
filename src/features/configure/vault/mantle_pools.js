ce4637a956b64af8869c1c1f12974b090aa905d0
VaporwaveFinance <95114728+VaporwaveFinance@users.noreply.github.com>
2023-02-03 12:22:42 -0500

    Create mantle_pools.js

---
+++ src/features/configure/vault/mantle_pools.js
@@ -0,0 +1,33 @@
+export const auroraPools = [
+ {
+    id: 'mantleTestnet-bit-test',
+    name: 'BIT-TEST PhotonLP',
+    token: 'BIT-TEST PhotonLP',
+    tokenDescription: 'Photonswap',
+    tokenAddress: '0xBBf3D4281F10E537d5b13CA80bE22362310b2bf9', //  wantToken
+    tokenDecimals: 18,
+    tokenDescriptionUrl: '#',
+    earnedToken: 'vapor-BIT-TEST-photonswap',
+    earnedTokenAddress: '0xf2aa2Df5b94B5e19aCac158BC0ee1945eB5DdC08', //  vault
+    earnContractAddress: '0xf2aa2Df5b94B5e19aCac158BC0ee1945eB5DdC08', //  vault
+    pricePerFullShare: 1,
+    tvl: 0,
+    oracle: 'lps',
+    oracleId: 'mantleTestnet-bit-test',
+    oraclePrice: 0,
+    depositsPaused: false,
+    status: 'active',
+    platform: 'Photonswap',
+    assets: ['BIT', 'TEST'],
+    risks: [
+      'COMPLEXITY_LOW',
+      'BATTLE_TESTED',
+      'IL_NONE',
+      'MCAP_MEDIUM',
+      'AUDIT',
+      'CONTRACTS_VERIFIED',
+    ],
+    stratType: 'StratLP',
+    withdrawalFee: '0.1%',
+  },
+ ];
